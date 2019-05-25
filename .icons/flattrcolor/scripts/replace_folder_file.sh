2d2482e3e730a1fe977e7781a836ca6042cfaefb
creio <mailcreio@gmail.com>
2019-05-26 01:17:25 +0300

    2019.05.26 Update

--- .icons/flattrcolor/scripts/replace_folder_file.sh
+++ .icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#1d2e35
-newglyph=#182a32
+oldglyph=#4d5d70
+newglyph=#4d5d70
 
 #	Front
 #	default color: 36d7b7
-oldfront=#3a5e6b
-newfront=#315767
+oldfront=#839ebe
+newfront=#839ebe
 
 #	Back
 #	default color: 1ba39c
-oldback=#284049
-newback=#223c47
+oldback=#5a6c82
+newback=#5a6c82
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
