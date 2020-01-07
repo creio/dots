fe6ec07bbd4c48634b07615096602c47a0acea11
creio <mailcreio@gmail.com>
2020-01-07 22:21:45 +0300

    2020.01.07 Update yay.sh

--- .bin/yay.sh
+++ .bin/yay.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Install script yay
-# autor: Alex Creio https://cvc.hashbase.io/
+# autor: Alex Creio https://creio.github.io/
 
 # wget git.io/yay.sh
 # sh yay.sh
 
-sudo pacman -S --noconfirm --needed wget curl git 
+sudo pacman -S --noconfirm --needed wget curl git
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 # makepkg -si
