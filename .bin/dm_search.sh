30ccd7f8a94ef41f086d6fd49fa54bb5e1f3c699
Trbl <domi@outlook.at>
2020-11-07 09:17:41 +0100

    Update dm_search.sh
    added support for special characters by properly encoding the query.

--- .bin/dm_search.sh
+++ .bin/dm_search.sh
@@ -11,9 +11,26 @@ SB='#5A74CA'
 # Foreground Prompt
 SF='#FFF'
 
+urlencode() {
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            ' ') printf "%%20" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+    LC_COLLATE=$old_lc_collate
+}
+
+
 # URL='https://www.startpage.com/do/search?hl=pt&q='
 URL='https://duckduckgo.com/?q='
-QUERY=$(echo '' | dmenu -fn $FN -i -p "Search: " -nb $NB -nf $NF -sb $SB -sf $SF)
+QUERY=$(urlencode "$(echo '' | dmenu -fn $FN -i -p "Search: " -nb $NB -nf $NF -sb $SB -sf $SF)")
 
 if [ -n "$QUERY" ]; then
 	xdg-open "${URL}${QUERY}" 2> /dev/null
